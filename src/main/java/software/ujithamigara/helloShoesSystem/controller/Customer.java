00ab206453a9c837a8a85d10a7824da096c5b387
Ujitha <ujithamigara@gmail.com>
2024-04-21 10:56:56 +0530

    add annotations

--- src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
@@ -1,4 +1,16 @@
 package software.ujithamigara.helloShoesSystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class Customer {
+
+    @GetMapping("/health")
+    public String healthTest(){
+        return "Customer Health Test";
+    }
+
 }
