b6c58298d41dc8663fa748a3ea0c0f4d86cbdc57
Ujitha <ujithamigara@gmail.com>
2024-04-21 10:57:54 +0530

    add mapping bean

--- src/main/java/software/ujithamigara/helloShoesSystem/HelloShoesSystemApplication.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/HelloShoesSystemApplication.java
@@ -1,11 +1,17 @@
 package software.ujithamigara.helloShoesSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloShoesSystemApplication {
 
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloShoesSystemApplication.class, args);
 	}
