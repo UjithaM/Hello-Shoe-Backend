13deab5549da82cf16122b5ca378cac5792cf734
Ujitha <ujithamigara@gmail.com>
2024-04-22 11:41:21 +0530

    implement CRUD

--- src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
@@ -1,9 +1,13 @@
 package software.ujithamigara.helloShoesSystem.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import software.ujithamigara.helloShoesSystem.dto.CustomerDTO;
 import software.ujithamigara.helloShoesSystem.service.CustomerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -18,5 +22,29 @@ public class Customer {
     @PostMapping
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customer) {
         return customerService.saveCustomer(customer);
+    }
+
+
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<CustomerDTO> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public CustomerDTO getCustomerById(@PathVariable String id) {
+        return customerService.getSelectedCustomer(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCustomer(@PathVariable String id, @RequestBody CustomerDTO customerDTO) {
+        customerService.updateCustomer(id, customerDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable String id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok().build();
     }
 }
