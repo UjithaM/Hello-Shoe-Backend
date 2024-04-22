bd9543480508369f7d3b2966bb31d0d2d62ee1b9
Ujitha <ujithamigara@gmail.com>
2024-04-22 11:32:31 +0530

    update with customer

--- src/main/java/software/ujithamigara/helloShoesSystem/util/Mapping.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/util/Mapping.java
@@ -3,7 +3,9 @@ package software.ujithamigara.helloShoesSystem.util;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+import software.ujithamigara.helloShoesSystem.dto.CustomerDTO;
 import software.ujithamigara.helloShoesSystem.dto.UserDTO;
+import software.ujithamigara.helloShoesSystem.entity.CustomerEntity;
 import software.ujithamigara.helloShoesSystem.entity.UserEntity;
 
 import java.util.List;
@@ -12,6 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Mapping {
     private final ModelMapper mapper;
+    public CustomerDTO toCustomerDTO(CustomerEntity customer) {
+        return  mapper.map(customer, CustomerDTO.class);
+    }
+    public CustomerEntity toCustomer(CustomerDTO customerDTO) {
+        return  mapper.map(customerDTO, CustomerEntity.class);
+    }
+    public List<CustomerDTO> toCustomerDTOList(List<CustomerEntity> customers) {
+        return mapper.map(customers, List.class);
+    }
     //UserMapping
     public UserEntity toUserEntity(UserDTO userDTO) {
         return mapper.map(userDTO, UserEntity.class);
