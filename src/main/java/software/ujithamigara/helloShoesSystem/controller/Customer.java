bb17b451af7279fb14a7e272fc0d1479f745cd14
Ujitha <ujithamigara@gmail.com>
2024-04-22 11:31:38 +0530

    update with customer save

--- src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/controller/Customer.java
@@ -1,16 +1,22 @@
 package software.ujithamigara.helloShoesSystem.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import software.ujithamigara.helloShoesSystem.dto.CustomerDTO;
+import software.ujithamigara.helloShoesSystem.service.CustomerService;
 
 @RestController
 @RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
 public class Customer {
-
+    private final CustomerService customerService;
     @GetMapping("/health")
     public String healthTest(){
         return "Customer Health Test";
     }
 
+    @PostMapping
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customer) {
+        return customerService.saveCustomer(customer);
+    }
 }
