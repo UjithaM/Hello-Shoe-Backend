7ed735bda97548ed709e1e458b24da038581c9f0
Ujitha <ujithamigara@gmail.com>
2024-04-30 12:54:18 +0530

    add OrderEntity table and implement it

---
+++ src/main/java/software/ujithamigara/helloShoesSystem/entity/OrderEntity.java
@@ -0,0 +1,30 @@
+package software.ujithamigara.helloShoesSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
+@Entity
+public class OrderEntity implements SuperEntity{
+    @Id
+    private String orderNo;
+    private String purchaseDate;
+    private String paymentMethod;
+    private double totalPrice;
+    private int itemQuantity;
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    private List<Order_item> orderItems;
+    @ManyToOne
+    @JoinColumn(name = "customerCode", referencedColumnName = "customerCode")
+    private CustomerEntity customerEntity;
+    @ManyToOne
+    @JoinColumn(name = "employeeCode", referencedColumnName = "employeeCode")
+    private EmployeeEntity employeeEntity;
+}
