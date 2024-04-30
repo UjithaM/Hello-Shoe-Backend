305633f67dc875421a69eb2e00790252d116c0aa
Ujitha <ujithamigara@gmail.com>
2024-04-30 12:54:41 +0530

    update supplierDto fields

--- src/main/java/software/ujithamigara/helloShoesSystem/dto/SupplierDTO.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/dto/SupplierDTO.java
@@ -1,10 +1,15 @@
 package software.ujithamigara.helloShoesSystem.dto;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import software.ujithamigara.helloShoesSystem.entity.ItemEntity;
 import software.ujithamigara.helloShoesSystem.entity.enums.Category;
+
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -12,13 +17,13 @@ public class SupplierDTO {
     private String supplierCode;
     private String name;
     private Category category;
-    private String addressLine01;
-    private String addressLine02;
-    private String addressLine03;
-    private String addressLine04;
-    private String addressLine05;
-    private String addressLine06;
-    private String contactNumber1;
-    private String contactNumber2;
+    private String addressNo;
+    private String lane;
+    private String mainCity;
+    private String mainState;
+    private String postalCode;
+    private String country;
+    private String mobileNumber;
+    private String landlineNumber;
     private String email;
 }
