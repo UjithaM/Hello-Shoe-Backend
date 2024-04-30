75cd9f5fe202581d8c52bdb05b99d7bb2317f816
Ujitha <ujithamigara@gmail.com>
2024-04-30 12:53:01 +0530

    add implement EmployeeEntity

---
+++ src/main/java/software/ujithamigara/helloShoesSystem/entity/EmployeeEntity.java
@@ -0,0 +1,44 @@
+package software.ujithamigara.helloShoesSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import software.ujithamigara.helloShoesSystem.entity.enums.Gender;
+import software.ujithamigara.helloShoesSystem.entity.enums.Role;
+
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "employee")
+@Entity
+public class EmployeeEntity {
+    @Id
+    private String employeeCode;
+    private String name;
+    private String profilePicture;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private String civilStatus;
+    private String Designation;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private Date dob;
+    private Date joinedDate;
+    private String attachedBranch;
+    private String addressNo;
+    private String lane;
+    private String mainCity;
+    private String mainState;
+    private String postalCode;
+    private String contactNumber;
+    @Column(unique = true)
+    private String email;
+    private String guardianName;
+    private String guardianContact;
+    @OneToMany(mappedBy = "employeeEntity")
+    private List<OrderEntity> orderEntities;
+}
