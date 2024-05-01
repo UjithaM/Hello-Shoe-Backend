a4cc5e74ac6271f752bacd0c70aa3a10c193839c
Ujitha <ujithamigara@gmail.com>
2024-05-01 11:51:44 +0530

    add ItemDTO and implement

---
+++ src/main/java/software/ujithamigara/helloShoesSystem/dto/ItemDTO.java
@@ -0,0 +1,26 @@
+package software.ujithamigara.helloShoesSystem.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO {
+    private String itemCode;
+    private String itemDescription;
+    private String itemPicture;
+    private String itemCategory;
+    private int size;
+    private double unitPriceSell;
+    private double unitPriceBuy;
+    private double expectedProfit;
+    private double profitMargin;
+    private int quantity;
+    private String itemStatus;
+    private SupplierDTO supplierDTO;
+    private List<Order_itemDTO> orderItemDTOS;
+}
