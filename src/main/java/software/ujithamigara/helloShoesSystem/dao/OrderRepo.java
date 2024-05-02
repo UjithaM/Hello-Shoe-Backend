5ac4d2ddd5acc25498b0d6772caacd0389dd69d1
Ujitha <ujithamigara@gmail.com>
2024-05-02 11:30:40 +0530

    add annotations

--- src/main/java/software/ujithamigara/helloShoesSystem/dao/OrderRepo.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/dao/OrderRepo.java
@@ -1,7 +1,9 @@
 package software.ujithamigara.helloShoesSystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import software.ujithamigara.helloShoesSystem.entity.OrderEntity;
 
+@Repository
 public interface OrderRepo extends JpaRepository<OrderEntity, String> {
 }
