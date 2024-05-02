00d9a7bc23d8db4c809f23a7a41ec67550f41ce3
Ujitha <ujithamigara@gmail.com>
2024-05-02 11:29:58 +0530

    add annotations

--- src/main/java/software/ujithamigara/helloShoesSystem/dao/ItemRepo.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/dao/ItemRepo.java
@@ -1,7 +1,9 @@
 package software.ujithamigara.helloShoesSystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import software.ujithamigara.helloShoesSystem.entity.ItemEntity;
 
+@Repository
 public interface ItemRepo extends JpaRepository<ItemEntity, String> {
 }
