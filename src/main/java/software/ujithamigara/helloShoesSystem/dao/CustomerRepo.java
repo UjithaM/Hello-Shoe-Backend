08af0738ec370f42d58442adc9444f26b23e4b52
Ujitha <ujithamigara@gmail.com>
2024-05-02 11:29:53 +0530

    add annotations

--- src/main/java/software/ujithamigara/helloShoesSystem/dao/CustomerRepo.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/dao/CustomerRepo.java
@@ -2,7 +2,8 @@ package software.ujithamigara.helloShoesSystem.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import software.ujithamigara.helloShoesSystem.entity.CustomerEntity;
-
+@Repository
 public interface CustomerRepo extends JpaRepository<CustomerEntity,String> {
 }
--- src/main/java/software/ujithamigara/helloShoesSystem/dao/EmployeeRepo.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/dao/EmployeeRepo.java
@@ -1,7 +1,8 @@
 package software.ujithamigara.helloShoesSystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import software.ujithamigara.helloShoesSystem.entity.EmployeeEntity;
-
+@Repository
 public interface EmployeeRepo extends JpaRepository<EmployeeEntity,String> {
 }
