83c627264d0b7b7de5f9b5adf958ba9675ced81f
Ujitha <ujithamigara@gmail.com>
2024-05-29 23:22:11 +0530

    implement Exception

---
+++ src/main/java/software/ujithamigara/helloShoesSystem/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package software.ujithamigara.helloShoesSystem.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
