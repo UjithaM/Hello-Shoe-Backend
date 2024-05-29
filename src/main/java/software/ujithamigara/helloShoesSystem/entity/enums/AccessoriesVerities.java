b6427712e71e49b38e7ec1ceb778a94ff57c4088
Ujitha <ujithamigara@gmail.com>
2024-05-29 23:19:42 +0530

    change name convention

--- src/main/java/software/ujithamigara/helloShoesSystem/entity/enums/AccessoriesVerities.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/entity/enums/AccessoriesVerities.java
@@ -1,5 +1,5 @@
 package software.ujithamigara.helloShoesSystem.entity.enums;
 
 public enum AccessoriesVerities {
-    ShoeShampoo, PolishColorsAsBlack, PolishColorsAsBrown, PolishColorsAsDarkBrown, FullSocks, HalfSocks
+    SHOE_SHAMPOO, POLISH_BLACK, POLISH_BROWN, POLISH_DARK_BROWN, FULL_SOCKS, HALF_SOCKS
 }
