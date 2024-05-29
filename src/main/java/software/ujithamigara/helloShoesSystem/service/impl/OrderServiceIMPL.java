781b5bda381dd39f7012683194b33aaa396b0ac5
Ujitha <ujithamigara@gmail.com>
2024-05-29 16:03:34 +0530

    implement customer transactional things

--- src/main/java/software/ujithamigara/helloShoesSystem/service/impl/OrderServiceIMPL.java
+++ src/main/java/software/ujithamigara/helloShoesSystem/service/impl/OrderServiceIMPL.java
@@ -3,11 +3,15 @@ package software.ujithamigara.helloShoesSystem.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import software.ujithamigara.helloShoesSystem.dao.OrderRepo;
+import software.ujithamigara.helloShoesSystem.dao.*;
 import software.ujithamigara.helloShoesSystem.dto.OrderDTO;
+import software.ujithamigara.helloShoesSystem.dto.Order_itemDTO;
+import software.ujithamigara.helloShoesSystem.entity.CustomerEntity;
+import software.ujithamigara.helloShoesSystem.entity.enums.Level;
 import software.ujithamigara.helloShoesSystem.service.OrderService;
 import software.ujithamigara.helloShoesSystem.util.Mapping;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,11 +19,28 @@ import java.util.UUID;
 @Transactional
 @RequiredArgsConstructor
 public class OrderServiceIMPL implements OrderService {
-    public  final OrderRepo orderRepo;
-    public final Mapping mapping;
+    private  final OrderRepo orderRepo;
+    private final CustomerRepo customerRepo;
+    private final ItemRepo itemRepo;
+    private final AccessoriesRepo accessoriesRepo;
+    private final EmployeeRepo employeeRepo;
+    private final Mapping mapping;
     @Override
     public OrderDTO saveOrder(OrderDTO orderDTO) {
-        orderDTO.setOrderNo(UUID.randomUUID().toString());
+        long orderCode = orderRepo.count();
+        String orderCodeString = String.format("O%04d", orderCode + 1);
+        orderDTO.setOrderNo(orderCodeString);
+        CustomerEntity customer = customerRepo.findById(orderDTO.getCustomerCode()).get();
+
+        if (orderDTO.getTotalPrice() > 800) customer.setTotalPoints(customer.getTotalPoints() + 1);
+
+        if (customer.getTotalPoints() >= 200) customer.setLevel(Level.GOLD);
+        else if (customer.getTotalPoints() >= 100) customer.setLevel(Level.SILVER);
+        else if (customer.getTotalPoints() >= 50) customer.setLevel(Level.BRONZE);
+
+        customer.setRecentPurchaseDate((Timestamp) orderDTO.getPurchaseDate());
+
+        customerRepo.save(customer);
         return mapping.toOrderDTO(orderRepo.save(mapping.toOrderEntity(orderDTO)));
     }
 
